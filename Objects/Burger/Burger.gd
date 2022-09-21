a1d4935136734abe839d12961ab03b413bdc8004
Douglas C <douglasc.dev@gmail.com>
2022-09-21 19:31:09 -0300

    Play munch sound when hitting a burger

--- Objects/Burger/Burger.gd
+++ Objects/Burger/Burger.gd
@@ -5,7 +5,7 @@ extends Area2D
 # var a = 2
 # var b = "text"
 onready var notifier = $VisibilityNotifier2D
-
+onready var munch_player = $MunchPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,6 +19,7 @@ func reposition_ahead():
 
 func _on_BurgerArea_body_shape_entered(_body_rid, _body, _body_shape_index, _local_shape_index):
 	reposition_ahead()
+	munch_player.play(1.2)
 
 func _on_screen_exited():
 	reposition_ahead()
